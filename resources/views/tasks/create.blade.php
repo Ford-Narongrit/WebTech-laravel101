cc6b1fe7929f38ee7067b3630f3b5612018a7afa
ford-narongrit <narongrit.th@ku.th>
2021-09-14 12:37:29 +0700

    edit css

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -7,23 +7,23 @@
         @csrf
         <div class="text-xl space-y-2">
             <label for="name" class="text-2xl">Task Title</label>
-            <input type="text" name="title" class="w-full px-2 py-3 rounded-xl border" placeholder="Task title"
+            <input type="text" name="title" class="w-full px-4 py-3 rounded-xl border" placeholder="Task title"
                 autocomplete="off">
         </div>
         <div class="text-xl space-y-2">
             <label for="detail" class="text-2xl">Detail</label>
-            <textarea type="text" name="detail" rows="4" cols="50" class="w-full px-2 py-3 rounded-xl border"
+            <textarea type="text" name="detail" rows="4" cols="50" class="w-full px-4 py-3 rounded-xl border"
                 placeholder="detail" autocomplete="off"></textarea>
         </div>
         <div class="text-xl space-y-2">
             <label for="due_date" class="text-2xl block">Due_date</label>
-            <input type="date" name="due_date" class="px-2 py-3 rounded-xl border" placeholder="detail" autocomplete="off">
+            <input type="date" name="due_date" class="px-4 py-3 rounded-xl border" placeholder="detail" autocomplete="off">
         </div>
 
         <div class="text-xl space-y-2">
             <label for="tags" class="text-2xl block">Task Tags (separated with comma)</label>
-            <input type="text" name="tags" class="w-full px-2 py-3 rounded-xl border" autocomplete="off"
-                placeholder="detail">
+            <input type="text" name="tags" class="w-full px-4 py-3 rounded-xl border" autocomplete="off"
+                placeholder="tags">
         </div>
         <div class="flex justify-center">
             <button type="submit" class="bg-green-400 rounded-xl text-xl hover:bg-green-300 p-5">Add Task</button>
