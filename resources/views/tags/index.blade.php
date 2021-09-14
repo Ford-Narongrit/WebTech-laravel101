5fc130995a2f2f4f4a0a5bc3a23e1c1602f6684b
ford-narongrit <narongrit.th@ku.th>
2021-09-14 12:40:40 +0700

    edit css

--- resources/views/tags/index.blade.php
+++ resources/views/tags/index.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.main')
 
 @section('content')
-<div class="space-y-5">
-    <div class="text=2xl font-bold text-gray-900 sm:text-3xl">
-        Tags
+    <div class="space-y-5">
+        <div class="text=2xl font-bold text-gray-900 sm:text-3xl text-center">
+            Tags
+        </div>
+        <table class="shadow-lg mx-auto w-3/5">
+            <thead>
+                <tr class="text-md font-semibold text-left text-gray-900 bg-gray-300 uppercase border-b border">
+                    <th class="px-4 py-3">Tag name</th>
+                    <th class="px-4 py-3">Number of tasks</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tags as $tag)
+                    <tr>
+                        <td class="px-4 py-3 border">
+                            <a href="{{ route('tags.slug', ['slug' => $tag->name]) }}" class="hover:underline">
+                                {{ $tag->name }}
+                            </a>
+                        </td>
+                        <td class="px-4 py-3 border">
+                            {{ $tag->tasks()->count() }}
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-    <table class="w-full shadow-lg">
-        <thead>
-            <tr class="text-md font-semibold text-left text-gray-900 bg-gray-300 uppercase border-b border">
-                <th class="px-4 py-3">Tag name</th>
-                <th class="px-4 py-3">Number of tasks</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($tags as $tag)
-            <tr>
-                <td class="px-4 py-3 border">
-                    <a href="{{route('tags.slug', ['slug' => $tag->name ]) }}" class="hover:underline">
-                        {{$tag->name}}
-                    </a>
-                </td>
-                <td class="px-4 py-3 border">
-                    {{$tag->tasks()->count()}}
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
 @endsection
